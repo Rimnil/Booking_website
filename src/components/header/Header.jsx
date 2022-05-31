1e45cc8322f508bbb8bc22ca5971b0948e37e776
Reshmi <rimjhim11998@gmail.com>
2022-05-31 13:57:46 +0530

    2nd commit

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,25 +1,26 @@
-import "./header.css"
 import {
-    faBed,
-    faCalendarDays,
+  faBed,
+  faCalendarDays,
   faCar,
   faPerson,
   faPlane,
   faTaxi,
-  } from "@fortawesome/free-solid-svg-icons";
-  import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
- import { DateRange } from "react-date-range";
-import { useState } from "react";
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "./header.css";
+import { DateRange } from "react-date-range";
+import { useContext, useState } from "react";
 import "react-date-range/dist/styles.css"; // main css file
 import "react-date-range/dist/theme/default.css"; // theme css file
 import { format } from "date-fns";
 import { useNavigate } from "react-router-dom";
-
+import { SearchContext } from "../../context/SearchContext";
+import { AuthContext } from "../../context/AuthContext";
 
 const Header = ({ type }) => {
-    const [destination, setDestination] = useState("");
+  const [destination, setDestination] = useState("");
   const [openDate, setOpenDate] = useState(false);
-  const [date, setDate] = useState([
+  const [dates, setDates] = useState([
     {
       startDate: new Date(),
       endDate: new Date(),
@@ -34,6 +35,8 @@ const Header = ({ type }) => {
   });
 
   const navigate = useNavigate();
+  const { user } = useContext(AuthContext);
+
 
   const handleOption = (name, operation) => {
     setOptions((prev) => {
@@ -44,18 +47,21 @@ const Header = ({ type }) => {
     });
   };
 
+  const { dispatch } = useContext(SearchContext);
+
   const handleSearch = () => {
-    navigate("/hotels", { state: { destination, date, options } });
+    dispatch({ type: "NEW_SEARCH", payload: { destination, dates, options } });
+    navigate("/hotels", { state: { destination, dates, options } });
   };
 
   return (
-   <div className="header">
-    <div
+    <div className="header">
+      <div
         className={
           type === "list" ? "headerContainer listMode" : "headerContainer"
         }
       >
-       <div className="headerList">
+        <div className="headerList">
           <div className="headerListItem active">
             <FontAwesomeIcon icon={faBed} />
             <span>Stays</span>
@@ -76,17 +82,19 @@ const Header = ({ type }) => {
             <FontAwesomeIcon icon={faTaxi} />
             <span>Airport taxis</span>
           </div>
-         </div>
-          <h1 className="headerTitle">
+        </div>
+        {type !== "list" && (
+          <>
+            <h1 className="headerTitle">
               A lifetime of discounts? It's Genius.
             </h1>
             <p className="headerDesc">
               Get rewarded for your travels – unlock instant savings of 10% or
-              more with a free Travelocity account
+              more with a free Lamabooking account
             </p>
-            <button className="headerBtn">Sign in / Register</button>
+            { !user && <button className="headerBtn">Sign in / Register</button>}
             <div className="headerSearch">
-            <div className="headerSearchItem">
+              <div className="headerSearchItem">
                 <FontAwesomeIcon icon={faBed} className="headerIcon" />
                 <input
                   type="text"
@@ -94,23 +102,22 @@ const Header = ({ type }) => {
                   className="headerSearchInput"
                   onChange={(e) => setDestination(e.target.value)}
                 />
-                
               </div>
               <div className="headerSearchItem">
                 <FontAwesomeIcon icon={faCalendarDays} className="headerIcon" />
                 <span
                   onClick={() => setOpenDate(!openDate)}
                   className="headerSearchText"
-                >{`${format(date[0].startDate, "MM/dd/yyyy")} to ${format(
-                  date[0].endDate,
+                >{`${format(dates[0].startDate, "MM/dd/yyyy")} to ${format(
+                  dates[0].endDate,
                   "MM/dd/yyyy"
                 )}`}</span>
                 {openDate && (
                   <DateRange
                     editableDateInputs={true}
-                    onChange={(item) => setDate([item.selection])}
+                    onChange={(item) => setDates([item.selection])}
                     moveRangeOnFirstSelection={false}
-                    ranges={date}
+                    ranges={dates}
                     className="date"
                     minDate={new Date()}
                   />
@@ -190,14 +197,17 @@ const Header = ({ type }) => {
                   </div>
                 )}
               </div>
-         <div className="headerSearchItem">
-             <button className="headerBtn" onClick={handleSearch}>Search</button>
-         </div>
-   </div>
-   </div>
+              <div className="headerSearchItem">
+                <button className="headerBtn" onClick={handleSearch}>
+                  Search
+                </button>
+              </div>
+            </div>
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
 
-   </div>
-   )
-}
-
-export default Header
+export default Header;
